f6b3c4e90a0b80428346d866bf2adabddfa7b485
Sherlock7X <101331517+Sherlock7X@users.noreply.github.com>
2025-06-12 11:02:07 +0000

    Deploying to gh-pages from @ Sherlock7X/Sherlock7X.github.io@0cfff94efe872a94f21a18a6346079525f82751d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -407,23 +407,22 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
+      },{id: "books-the-unbearable-lightness-of-being",
+          title: 'The Unbearable Lightness of Being',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+              window.location.href = "/books/UnbearableLightness/";
+            },},{id: "news-started-my-phd-journey-in-quantitative-finance-at-hkust",
+          title: 'Started my PhD Journey in Quantitative Finance at HKUST!',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_1/";
+            },},{id: "news-attended-9th-asian-quantitative-finance-conference",
+          title: 'Attended 9th Asian Quantitative Finance Conference!',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
